9b2673146cca2f4877bb83e7c18ff3199eef184a
Algeria2 <arak1433@mail.ru>
2025-08-26 19:31:35 +0500

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 
 def main():
-    # 1. Первый запрос - получаем первую страницу
     url = "https://catfact.ninja/facts?page=1"
     response = requests.get(url)
     data = response.json()
@@ -14,14 +13,12 @@ def main():
     print(f"Фактов на странице: {per_page}")
     print(f"Последняя страница: {last_page}")
 
-    # 2. Запрос последней страницы
     last_url = f"https://catfact.ninja/facts?page={last_page}"
     last_response = requests.get(last_url)
     last_data = last_response.json()
 
     facts = last_data["data"]
-
-    # 3. Поиск самого короткого факта
+    
     shortest_fact = min(facts, key=lambda x: len(x["fact"]))["fact"]
 
     print("\nСамый короткий факт на последней странице:")
